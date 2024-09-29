4bf4af13ed258ed16178eba1e2ee49ec4f16a23c
likeRudin <likeRudin@gmail.com>
2024-09-30 00:57:45 +0900

    Feat: add apps - users

--- mysite/tweets/models.py
+++ mysite/tweets/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from ..users.models import User
 
 # Create your models here.
+
+
+class Tweet(models.Model):
+    payload = models.TextField(max_length=180)
+    user = models.ForeignKey()
+    created_at = models.DateField()
+    updated_at = models.DateField()
+    
+    def __str__(self) -> str:
+        return f"{super().__str__()} Tweet Model"
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    updated_at = models.DateField()
+
+     
+    def __str__(self) -> str:
+        return f"{super().__str__()} LikeModel"
+
+
---
+++ mysite/users/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "username",
+                    "password",
+                    "name",
+                    "email",
+
+                ),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_superuser",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    list_display = ("username", "email", "name", )
